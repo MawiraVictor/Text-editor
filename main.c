1805f89c5bfd8c7b07ed9885ea7c823ce7136c10
MawiraVictor <Victor.Mawira@strathmore.edu>
2026-03-19 00:20:08 +0300

    error handling

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include<termios.h>
+#include<errno.h>
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<ctype.h>
 
 struct termios orig_termios;
+void die (const char *s) {
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &origin_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &origin_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
     
     struct termios raw = orig_termios;
@@ -22,7 +28,7 @@ void enableRawMode(){
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STD_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STD_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main (){
@@ -31,7 +37,7 @@ int main (){
     char c;
     while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {

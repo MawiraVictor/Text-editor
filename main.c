e46608925385e4e28f9d0e121726f733f806b8d5
MawiraVictor <Victor.Mawira@strathmore.edu>
2026-03-19 15:51:47 +0100

    clear screen on exit

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 struct termios orig_termios;
 
 void die (const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -56,6 +59,8 @@ void editorProcessKeypress(){
 
 	switch (c) {
 		case CTRL_KEY('q'):
+		 write(STDOUT_FILENO, "\x1b[2J", 4);
+		 write(STDOUT_FILENO, "\X1b[h", 3)
 		exit(0);
 		break;
 	}
